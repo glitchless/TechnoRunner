97392c0b64bbe0873f45639a72b1001add0a605d
LionZXY <nikita@kulikof.ru>
2019-05-19 18:38:39 +0300

    Test new script

---
+++ .travis/install_jdk_unix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+    brew tap AdoptOpenJDK/openjdk
+    brew cask install adoptopenjdk8
+elif [ $TRAVIS_OS_NAME = 'linux' ]; then
+    sudo add-apt-repository -y ppa:saiarcot895/myppa
+    sudo apt update
+    sudo apt install openjdk-8-jdk
+fi
