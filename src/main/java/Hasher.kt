9f7d7fd14b1c861a6dabcc3955a358749345285c
LionZXY <nikita@kulikof.ru>
2019-05-19 16:57:16 +0300

    Update deps

--- src/main/java/Hasher.kt
+++ src/main/java/Hasher.kt
@@ -1,6 +1,6 @@
 import utils.Utils
 import java.io.File
 
-fun main() {
-    println(Utils.generateSHA256(File("TechnoparkLauncher-1.0-SNAPSHOT.jar")))
+fun main(args: Array<String>) {
+    println(Utils.generateSHA256(File(args[0])))
 }
