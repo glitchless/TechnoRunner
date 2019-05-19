985295a0d2f8b998b9156be26cd111171e1332e3
Oleg Morozenkov <m@oleg.rocks>
2019-05-19 16:32:34 +0300

    WIP: Make pkg

---
+++ src/pkg/paths.h
@@ -0,0 +1,3 @@
+#define JRE_DIRNAME         "jre"
+#define JREPATH_FILENAME    "jrepath.txt"
+#define RUNNER_JAR_FILENAME "runner.jar"
---
+++ src/pkg/untar.h
@@ -0,0 +1,2 @@
+int
+untar(const void *buf, size_t buf_len, const char *dest_path);
