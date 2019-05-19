b06bb80dbed4b3fdeea5da8a0b9bdc9e36a14701
LionZXY <nikita@kulikof.ru>
2019-05-19 18:51:36 +0300

    Revert windows in script

--- .travis/install_jdk.sh
+++ .travis/install_jdk.sh
@@ -7,8 +7,4 @@ elif [ $TRAVIS_OS_NAME = 'linux' ]; then
     sudo add-apt-repository -y ppa:saiarcot895/myppa
     sudo apt update
     sudo apt install openjdk-8-jdk
-elif [ $TRAVIS_OS_NAME = 'windows' ]; then
-  git clone https://github.com/portapps/portapps ${TRAVIS_BUILD_DIR}/../portapps
-  source ${TRAVIS_BUILD_DIR}/../portapps/.travis/prepare.sh
-  bash ${TRAVIS_BUILD_DIR}/../portapps/.travis/java.sh
 fi
