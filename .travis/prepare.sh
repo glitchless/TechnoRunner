63adbb0ddea0cf122096fb592ed2cf4ed25b7478
LionZXY <nikita@kulikof.ru>
2019-05-19 19:02:26 +0300

    Try install zlib

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -3,8 +3,9 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     brew tap AdoptOpenJDK/openjdk
     brew cask install adoptopenjdk8
+    brew install zlib
 elif [ $TRAVIS_OS_NAME = 'linux' ]; then
     sudo add-apt-repository -y ppa:saiarcot895/myppa
     sudo apt update
-    sudo apt install openjdk-8-jdk
+    sudo apt install openjdk-8-jdk zlib
 fi
